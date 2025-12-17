42beb4a9cdf2dac32b45dc46e864a19c9de116f5
jaosalles <joaopedrosalles@hotmail.com>
2025-12-17 10:16:16 -0300

    ci(config,ci): :ferris_wheel: update ci configurations

---
+++ commitlint.config.mjs
@@ -0,0 +1,47 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  defaultIgnores: true,
+  ignores: [(message) => message.startsWith('Merge ')],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'deps'
+      ]
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'components',
+        'hooks',
+        'pages',
+        'lib',
+        'utils',
+        'styles',
+        'config',
+        'e2e',
+        'tests',
+        'docs',
+        'deps',
+        'ci'
+      ]
+    ],
+    // Permite commits sem escopo
+    'scope-empty': [0],
+    // Não força casing do assunto
+    'subject-case': [0]
+  }
+}
